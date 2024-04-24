1e08111704378133f7094091bcb9ac385263f097
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-24 16:02:55 +0200

    fine, scusate

--- javaScript/main.js
+++ javaScript/main.js
@@ -7,7 +7,18 @@ const lista = [
     "sigarette"
 ]
 
-for (let i = 0; i < lista.length; i++){
-    const elemento = lista[i]
-    console.log (elemento)
+// for (let i = 0; i < lista.length; i++){
+//     const elemento = lista[i]
+//     console.log (elemento)
+// }
+const ul = document.getElementById("my-ul");
+let i = 0;
+while (i < lista.length){
+    const elemento = lista[i];
+    const myLi = document.createElement("li");
+    myLi.textContent = elemento;
+    console.log (elemento);
+    i++;
+    ul.append(myLi)
 }
+i = null;
