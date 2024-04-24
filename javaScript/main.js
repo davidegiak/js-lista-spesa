232493580a25256f4718a54cc3c64f9b44c65c84
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-04-24 15:46:46 +0200

    first commit

---
+++ javaScript/main.js
@@ -0,0 +1,13 @@
+const lista = [
+    "pomodori",
+    "zucchine",
+    "pane",
+    "uova",
+    "pasta",
+    "sigarette"
+]
+
+for (let i = 0; i < lista.length; i++){
+    const elemento = lista[i]
+    console.log (elemento)
+}
